b69cb57ee422222ceb813b1eed8d2e8ce1551d7d
Dumitru Cernei <49617451+dumitru2002@users.noreply.github.com>
2023-06-08 22:19:01 +0300

    admin adduser implementation

--- CVBuilderAuth/Controllers/AdminController.cs
+++ CVBuilderAuth/Controllers/AdminController.cs
@@ -1,7 +1,6 @@
-﻿using CVBuilderAuth.Models.Domain;
-using CVBuilderAuth.Models.DTO;
+﻿using CVBuilderAuth.Models.DTO;
+using CVBuilderAuth.Repositories.Abstract;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CVBuilderAuth.Controllers
@@ -14,46 +13,25 @@ namespace CVBuilderAuth.Controllers
             return View();
         }
 
-
-        private readonly UserManager<ApplicationUser> _userManager;
-        private readonly RoleManager<IdentityRole> _roleManager;
-
-        public AdminController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        private readonly IUserAuthenticationService _service;
+        public AdminController(IUserAuthenticationService service)
         {
-            _userManager = userManager;
-            _roleManager = roleManager;
+            this._service = service;
         }
 
-        // Action method to handle the new user registration form submission
+        public IActionResult AddUser()
+        {
+            return View();
+        }
+
         [HttpPost]
         public async Task<IActionResult> AddUser(RegistrationModel model)
         {
-            if (ModelState.IsValid)
-            {
-                var user = new ApplicationUser { UserName = model.Username, Email = model.Email };
-
-                // Create the user
-                var result = await _userManager.CreateAsync(user, model.Password);
-
-                if (result.Succeeded)
-                {
-                    // Add the user to the selected role
-                    await _userManager.AddToRoleAsync(user, model.Role);
-
-                    return RedirectToAction("Index", "Home");
-                }
-                else
-                {
-                    // Handle the errors if the user creation fails
-                    foreach (var error in result.Errors)
-                    {
-                        ModelState.AddModelError(string.Empty, error.Description);
-                    }
-                }
-            }
-
-            // If the model state is not valid, return the registration view with validation errors
-            return View(model);
+            if (!ModelState.IsValid)
+                return View(model);
+            var result = await _service.RegistrationAsync(model);
+            TempData["msg"] = result.Message;
+            return RedirectToAction(nameof(Display));
         }
     }
 }
