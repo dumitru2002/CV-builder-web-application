5614c252c602e8c8db5993ba8ae074557c6c5a09
Dumitru Cernei <49617451+dumitru2002@users.noreply.github.com>
2023-06-10 19:23:31 +0300

    Dashboard changes

--- CVBuilderAuth/Controllers/DashboardController.cs
+++ CVBuilderAuth/Controllers/DashboardController.cs
@@ -1,14 +1,39 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using CVBuilderAuth.Models;
+using CVBuilderAuth.Models.Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace CVBuilderAuth.Controllers
 {
     [Authorize]
     public class DashboardController : Controller
     {
+        ApplicationContext db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public DashboardController(ApplicationContext context, UserManager<ApplicationUser> userManager)
+        {
+            db = context;
+            _userManager = userManager;
+        }
         public IActionResult Display()
         {
-            return View();
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            UserCvInfo userCvInfo = db.UserCvInfos.FirstOrDefault(u => u.UserId == userId);
+            if (userCvInfo == null)
+            {
+                
+                return View(true);
+            }
+            else
+            {
+               
+                return View(false);
+            }
         }
+
+        
+
     }
 }
