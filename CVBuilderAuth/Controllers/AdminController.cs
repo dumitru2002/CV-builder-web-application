fa4c8cc90c70bcb40be5fbe21bba44c3a8cddea8
Dumitru Cernei <49617451+dumitru2002@users.noreply.github.com>
2023-06-10 18:08:35 +0300

    User delete sevice implementation

--- CVBuilderAuth/Controllers/AdminController.cs
+++ CVBuilderAuth/Controllers/AdminController.cs
@@ -60,5 +60,36 @@ namespace CVBuilderAuth.Controllers
             var users = db.Users.ToList();
             return View(users);
         }
+
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteUser(string id)
+        {
+            var user = await userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = $"User with Id = {id} cannot be found";
+                return View("NotFound");
+            }
+            else
+            {
+                var result = await userManager.DeleteAsync(user);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListUsers");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View("ListUsers");
+            }
+        }
     }
 }
